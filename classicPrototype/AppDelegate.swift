0e48dd60250fbd9936d7037f283ddc07c0900dee
yuzgit <ofnmk.xcode.05@gmail.com>
2021-09-08 15:14:50 +0900

    Initial Commit

---
+++ classicPrototype/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  classicPrototype
+//
+//  Created by atelier on 2021/09/08.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ classicPrototype/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  classicPrototype
+//
+//  Created by atelier on 2021/09/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
